281c570910481bc0cd3c34b48d82e8b2e04672eb
mew-ton <mxl_usmarc@slmail.mewton.jp>
2024-05-17 14:47:07 +0900

    fix: biome failed

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,5 +1,5 @@
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema'
 import { defineCollection } from 'astro:content'
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema'
 
 export const collections: Record<string, ReturnType<typeof defineCollection>> = {
   docs: defineCollection({ schema: docsSchema() }),
